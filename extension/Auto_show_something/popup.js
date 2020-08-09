f50b7ea0c7b4a0e9fd90ae09f724677fb126e3fe
lampart_vi_hoang_son <vihoangson@gmail.com>
2020-08-09 14:30:21 +0700

    upload extension

---
+++ extension/Auto_show_something/popup.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+	
+	$("button").click(function () {
+		$.get('http://ttht.oop.vn', (dataRandom) => {
+			// let dataRandom = data.rows[Math.floor(Math.random() * data.rows.length)]
+			clickbutton(dataRandom);
+        })
+
+	})
+	function clickbutton(dataRandom) {
+		$("#result-ttht").html('');
+		dataRandom.forEach((v) => {
+			let q = $("<div class='question'></div>").append(v.q);
+			let a = $("<div class='answer'></div>").append(v.a);			
+			$("#result-ttht").append(q);
+			$("#result-ttht").append(a);
+			return;
+		})
+	}
+});
+/*$.get('https://jsonplaceholder.typicode.com/todos/1', function (d) {
+    alert(JSON.stringify(d) );
+})*/
