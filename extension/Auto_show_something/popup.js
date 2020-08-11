201d831c151f96690c80d33ad5430a66a6868fe5
[Lampart] Vi Hoang Son <hoang_son@lampart-vn.com>
2020-08-11 11:34:10 +0700

    upload extension

--- extension/Auto_show_something/popup.js
+++ extension/Auto_show_something/popup.js
@@ -1,8 +1,7 @@
 ﻿$(document).ready(function () {
 	
 	$("button").click(function () {
-		$.get('http://ttht.oop.vn', (dataRandom) => {
-			// let dataRandom = data.rows[Math.floor(Math.random() * data.rows.length)]
+		$.get('http://ttht.oop.vn', (dataRandom) => {			
 			clickbutton(dataRandom);
         })
 
@@ -14,10 +13,51 @@
 			let a = $("<div class='answer'></div>").append(v.a);			
 			$("#result-ttht").append(q);
 			$("#result-ttht").append(a);
+		
+
+			var settingss = {
+			  "url": "https://api.chatwork.com/v2/rooms/119727315/messages",
+			  "method": "POST",
+			  "timeout": 0,
+			  "headers": {
+			    "X-ChatWorkToken": "6598c5b05c7c3a1508f35fe465474caf",
+			    "Content-Type": "application/x-www-form-urlencoded"
+			  },
+	          beforeSend: function (request){
+	            request.withCredentials = false;            
+	          },
+			  "data": {
+			    "body": "Câu hỏi:\n"+v.q +" \n\n Trả lời:\n "+  v.a
+			  }
+			};
+
+			$.ajax(settingss).done(function (response) {
+			  console.log(response);
+			});
+
+			var settings = {
+			  "url": "https://172.16.2.38:8443/api/v1/message/sent",
+			  "method": "POST",
+			  "timeout": 0,
+			  "headers": {
+			    "secret": "c87f99f610928a03b57ca00c7ffb2750",
+			    "userid": "90",
+			    "Content-Type": "application/json"
+			  },
+	        	beforeSend: function (request){
+	            request.withCredentials = false;            
+	          },
+			  "data": JSON.stringify({"room_id":90,"type":0,"message":"Câu hỏi:\n"+v.q +" \n\n Trả lời:\n "+  v.a}),
+			};
+
+			$.ajax(settings).done(function (response) {
+			  console.log(response);
+			});
+
 			return;
+
+
 		})
 	}
+	$("button").trigger('click');
 });
-/*$.get('https://jsonplaceholder.typicode.com/todos/1', function (d) {
-    alert(JSON.stringify(d) );
-})*/
